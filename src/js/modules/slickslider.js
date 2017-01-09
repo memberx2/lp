d060a100f43bf07306df39d7e4036aa83e9dd5dd
Joanna <asiad706@gmail.com>
2017-01-09 09:03:26 +0100

    slider and button fixes

--- src/js/modules/slickslider.js
+++ src/js/modules/slickslider.js
@@ -4,7 +4,10 @@ const SlickSlider = {
             $('.slider').slick({
                 infinite: true,
                 slidesToShow: 3,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2700,
+                arrows: false,
             });
         });
     }
